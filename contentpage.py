6a09e77882421a425b0bcd03eb0a2a80d475413c
Ali Çine <vodbwi@gmail.com>
2025-08-02 17:49:43 +0300

    Add files via upload

--- contentpage.py
+++ contentpage.py
@@ -3,10 +3,6 @@ import os
 
 cntr = ft.MainAxisAlignment.CENTER
 
-class ValueInput(ft.Container):
-    def __init__(self, master):
-        pass
-
 class ContentPage(ft.View):
     def __init__(self, master):
 
@@ -17,20 +13,8 @@ class ContentPage(ft.View):
         self.bw = 25 
 
         self.initialize_content()
-        self.container = ft.Container(content=self.colon)
-        appbar = ft.AppBar(
-                title=ft.Text("Password Wallet"),
-                center_title=False,
-                bgcolor=ft.Colors.SURFACE_CONTAINER_HIGHEST,
-                actions=[
-                    ft.PopupMenuButton(
-                        items=[
-                            ft.PopupMenuItem(text="Quit", icon=ft.Icons.SENSOR_DOOR_OUTLINED, on_click=lambda _: self.page.window.destroy()),
-                            ]
-                        ),
-                    ],
-                )
-        super().__init__(controls=[self.container], horizontal_alignment=ft.alignment.center, vertical_alignment=ft.MainAxisAlignment.CENTER, scroll=ft.ScrollMode.ALWAYS, appbar=appbar)
+        self.container = ft.Container(content=self.colon, expand=True, margin=ft.margin.symmetric(horizontal=40))
+        super().__init__(controls=[self.container], horizontal_alignment=ft.CrossAxisAlignment.CENTER, vertical_alignment=ft.MainAxisAlignment.CENTER, scroll=ft.ScrollMode.ALWAYS)
 
     def initialize_content(self):
         self.colon.controls = []
@@ -39,7 +23,7 @@ class ContentPage(ft.View):
                 ft.DataColumn(ft.Text("Note", expand=True)),
                 ft.DataColumn(ft.Text("Key", expand=True)),
                 ft.DataColumn(ft.Text("Value", expand=True)),
-                ft.DataColumn(ft.Text("Show")),
+                ft.DataColumn(ft.Text("Show", expand=True)),
                 ]
         rows = []
         for i in range(0,100):
@@ -53,27 +37,28 @@ class ContentPage(ft.View):
                      ft.DataCell(
                          ft.Row([
                          ft.TextField(value=f'Keey {i}j', disabled=True, password=True, expand=True),
-                         ft.IconButton(icon=ft.Icons.EDIT, on_click=lambda _, g=i, n=2: self.edit_callback(g,n)),
+                         ft.IconButton(icon=ft.Icons.EDIT, on_click=lambda _, g=i, n=1: self.edit_callback(g,n)),
                          ])
                          ),
                      ft.DataCell(
                          ft.Row([
                          ft.TextField(value="Vaal {i}", disabled=True, password=True, expand=True),
-                         ft.IconButton(icon=ft.Icons.EDIT, on_click=lambda _, g=i, n=4: self.edit_callback(g,n)),
+                         ft.IconButton(icon=ft.Icons.EDIT, on_click=lambda _, g=i, n=2: self.edit_callback(g,n)),
                          ])
                          ),
                      ft.DataCell(
                          ft.IconButton(icon=ft.Icons.PREVIEW, on_click=lambda _, g=i: self.show_callback(g), expand=True)
                          )]
             dtrow = ft.DataRow(cells=cells)
+            setattr(self, f'row_group{i}', dtrow)
+
 
             rows.append(dtrow)
+            setattr(self, f'cell_{i}0_editing', False)
+            setattr(self, f'cell_{i}1_editing', False)
+            setattr(self, f'cell_{i}2_editing', False)
 
-            setattr(self, f'row_group{i}_0_editing', False)
-            setattr(self, f'row_group{i}_2_editing', False)
-            setattr(self, f'row_group{i}_4_editing', False)
-
-        self.dt = ft.DataTable(columns=cols, rows=rows, expand=True)
+        self.dt = ft.Row([ft.DataTable(columns=cols, rows=rows, expand=True)], alignment=ft.MainAxisAlignment.CENTER)
         self.colon.controls.append(self.dt)
         self.master.update()
 
@@ -81,10 +66,10 @@ class ContentPage(ft.View):
         i = g
         row_group = getattr(self, f'row_group{i}')
 
-        key_control = row_group.controls[2]
+        key_control = row_group.cells[1].content.controls[0]
         key_control.password = not key_control.password
 
-        value_control = row_group.controls[4]
+        value_control = row_group.cells[2].content.controls[0]
         value_control.password = not value_control.password
 
         self.master.update()
@@ -92,29 +77,27 @@ class ContentPage(ft.View):
     def edit_callback(self, g:int, n:int):
         i = g
         row_group = getattr(self, f'row_group{i}')
-        row_controls = row_group.controls
-        is_editing = getattr(self, f'row_group{i}_{n}_editing')
+        row_cells = row_group.cells
+        is_editing = getattr(self, f'cell_{i}{n}_editing')
 
+        new_row_control = row_cells[n].content.controls[0]
         if is_editing:
-            new_row_control = row_controls[n]
             new_row_control.disabled = True
-            row_controls[n] = new_row_control
+            row_cells[n].content.controls[0] = new_row_control
 
-            row_edit_control = row_controls[n+1]
+            row_edit_control = row_cells[n].content.controls[1]
             row_edit_control.icon = ft.Icons.EDIT
-            row_controls[n+1] = row_edit_control
+            row_cells[n].content.controls[1] = row_edit_control
         else:
-
-            new_row_control = row_controls[n]
             new_row_control.disabled = False
-            row_controls[n] = new_row_control
+            row_cells[n].content.controls[0] = new_row_control
 
-            row_edit_control = row_controls[n+1]
+            row_edit_control = row_cells[n].content.controls[1] 
             row_edit_control.icon = ft.Icons.CLOSE
-            row_controls[n+1] = row_edit_control
+            row_cells[n].content.controls[1] = row_edit_control
 
-        row_group.controls = row_controls
-        setattr(self, f'row_group{i}_{n}_editing', not is_editing)
+        row_group.cells = row_cells
+        setattr(self, f'cell_{i}{n}_editing', not is_editing)
         setattr(self, f'row_group{i}', row_group)
         self.master.update()
 
@@ -125,8 +108,26 @@ class MainWindow:
         self.page.title = "Content Page"
         self.page.vertical_alignment = cntr 
         self.page.on_route_change = self.route_change
+
+        dlg = ft.AlertDialog(modal=True,title=ft.Text("Info"), content=ft.Text("To encrypt data in the app, you need to register before you can start storing it.\n This app does not communicate with the internet, everything is stored locally. Content is encrypted and decrypted using your master password, master password is not stored as plain text.\n\nData is stored\n\nOn Linux:\n'~/.config/password_wallet'\n\nOn macOS:\n'/Users/<username>/Library/Application Support/password_wallet'\n\nOn Windows:\n'C:\\Users\\<username>\\AppData\\Local\\duzdunya\\password_wallet'\n\nAuthor: Ali Çine"), actions=[ft.TextButton("Ok",on_click=lambda _: self.master.close(dlg))])
+        self.apb= ft.AppBar(
+                title=ft.Text("Password Wallet"),
+                center_title=False,
+                bgcolor=ft.Colors.SURFACE_CONTAINER_HIGHEST,
+                actions=[
+                    ft.PopupMenuButton(
+                        items=[
+                        ft.PopupMenuItem(text="Info", icon=ft.Icons.INFO, on_click=lambda _: self.page.open(dlg)),
+                            ft.PopupMenuItem(text="Quit", icon=ft.Icons.SENSOR_DOOR_OUTLINED, on_click=lambda _: self.page.window.destroy()),
+                            ]
+                        ),
+                    ],
+                )
+
+        self.page.update()
         self.page.go("/")
 
+        
     def route_change(self, route):
         self.page.views.clear()
 
