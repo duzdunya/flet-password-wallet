3e189849fe876867b933f9a043ceedced2e6e60e
cineali <vodbwi@gmail.com>
2025-08-15 20:49:23 +0300

    ui colors changed

--- content/loginpage.py
+++ content/loginpage.py
@@ -8,16 +8,15 @@ from sec.auth import login_check
 from sec.encryption import get_user_key, decrypt_the_content
 
 
-
 class LoginPage(ft.View):
     def __init__(self, master: "MainWindow", *args, **kwargs):
         self.master = master
         self.l = self.master.l
-        self.header = ft.Text(self.l.welcome, size=20)
-        self.username_entry = ft.TextField(label=self.l.username, width=300 )
-        self.password_entry = ft.TextField(label=self.l.password, password=True, can_reveal_password=True, width=300)
-        self.login_button= ft.ElevatedButton(text=self.l.login, on_click=lambda _:self.login_callback(), style=ft.ButtonStyle(shape=ft.RoundedRectangleBorder(radius=0)), width=300)
-        self.register_button = ft.ElevatedButton(text=self.l.register, on_click=lambda _:self.register_callback(),style=ft.ButtonStyle(shape=ft.RoundedRectangleBorder(radius=0)), width=300)
+        self.header = ft.Text(self.l.welcome, size=30, font_family="noto sans")
+        self.username_entry = ft.TextField(label=self.l.username, width=300, border_color=ft.Colors.WHITE54)
+        self.password_entry = ft.TextField(label=self.l.password, password=True, can_reveal_password=True, width=300, border_color=ft.Colors.WHITE54)
+        self.login_button= ft.ElevatedButton(text=self.l.login, on_click=lambda _:self.login_callback(), style=ft.ButtonStyle(shape=ft.RoundedRectangleBorder(radius=0)), width=300, color=ft.Colors.WHITE70)
+        self.register_button = ft.ElevatedButton(text=self.l.register, on_click=lambda _:self.register_callback(),style=ft.ButtonStyle(shape=ft.RoundedRectangleBorder(radius=0)), width=300, color=ft.Colors.WHITE70)
 
         self.colon = ft.Column([
             self.header,
@@ -25,15 +24,16 @@ class LoginPage(ft.View):
             self.password_entry,
             self.login_button,
             self.register_button
-            ], alignment=CNTR, horizontal_alignment=CX_CENTER)
+            ],
+            alignment=CNTR,
+            horizontal_alignment=CX_CENTER,
+                               expand=True
+                               )
 
-        self.appbar = CustomAppBar(title="Password Wallet | Login", master=self.master)
-
-        self.container = ft.Container(content=self.colon, alignment=ft.alignment.center, bgcolor=ft.Colors.SURFACE_CONTAINER_HIGHEST, padding=30)
-
-        self.v_row = ft.ResponsiveRow([ ft.Column([ self.container ],col={"xs":12, "md":6}) ], alignment=CNTR)
-
-        super().__init__(controls=[self.v_row], horizontal_alignment=ft.CrossAxisAlignment.CENTER, vertical_alignment=ft.MainAxisAlignment.CENTER, appbar=self.appbar)
+        self.appbar = CustomAppBar(title=self.l.pw_login, master=self.master)
+        self.v_row = ft.ResponsiveRow([ft.Column([ft.Container(content=self.colon, bgcolor=ft.Colors.with_opacity(0.1, ft.Colors.WHITE), blur=4, padding=ft.padding.symmetric(50, 120))], col={"xs":12, "md":6}, alignment=CNTR, horizontal_alignment=CX_CENTER)], alignment=ft.MainAxisAlignment.CENTER, expand=True)
+#ft.Container(content=self.v_row, alignment=CNTR, expand=True, image=ft.DecorationImage(src="./carbon.jpg", fit=ft.ImageFit.COVER))
+        super().__init__(padding=0,controls=[ft.Container(content=self.v_row, expand=True, image=ft.DecorationImage(src="./carbon.jpg", opacity=0.5, fit=ft.ImageFit.COVER),alignment=ft.Alignment(x=0.5, y=0.5))], horizontal_alignment=ft.CrossAxisAlignment.CENTER, vertical_alignment=ft.MainAxisAlignment.CENTER, appbar=self.appbar)
 
     # mainly used in Appbar
     def __str__(self):
--- content/registerpage.py
+++ content/registerpage.py
@@ -13,13 +13,13 @@ class RegisterPage(ft.View):
 
         self.masterpage = master.page
 
-        self.signup = ft.Text("Sign Up", size=20)
-        self.name_entry = ft.TextField(label=self.l.name, width=300)
-        self.username_entry = ft.TextField(label=self.l.username, width=300)
-        self.password_entry = ft.TextField(label=self.l.password, width=300, password=True, can_reveal_password=True)
-        self.password_two_entry = ft.TextField(label=self.l.password_again, width=300, password=True, can_reveal_password=True)
-        self.submit = ft.ElevatedButton(self.l.submit, style=ft.ButtonStyle(shape=ft.RoundedRectangleBorder(radius=0)), width=300, on_click= lambda _: self.register_callback())
-        self.back = ft.IconButton(on_click=lambda _: self.master.page.go("/login"), icon=ft.Icons.KEYBOARD_RETURN)
+        self.signup = ft.Text("Sign Up", size=30, font_family="noto sans")
+        self.name_entry = ft.TextField(label=self.l.name, width=300, border_color=ft.Colors.WHITE54)
+        self.username_entry = ft.TextField(label=self.l.username, width=300, border_color=ft.Colors.WHITE54)
+        self.password_entry = ft.TextField(label=self.l.password, width=300, password=True, can_reveal_password=True, border_color=ft.Colors.WHITE54)
+        self.password_two_entry = ft.TextField(label=self.l.password_again, width=300, color=ft.Colors.WHITE70, password=True, can_reveal_password=True, border_color=ft.Colors.WHITE54)
+        self.submit = ft.ElevatedButton(self.l.submit, color=ft.Colors.WHITE70, style=ft.ButtonStyle(shape=ft.RoundedRectangleBorder(radius=0)), width=300, on_click= lambda _: self.register_callback())
+        self.back = ft.IconButton(on_click=lambda _: self.master.page.go("/login"), icon_color=ft.Colors.WHITE70, icon=ft.Icons.KEYBOARD_RETURN)
 
         self.colon = ft.Column([
             self.signup,
@@ -31,12 +31,13 @@ class RegisterPage(ft.View):
             self.back
             ], alignment=CNTR, horizontal_alignment=CX_CENTER)
 
-        self.appbar = CustomAppBar(title="Password Wallet | Register", master=self.master)
+        self.appbar = CustomAppBar(title=self.l.pw_register, master=self.master)
 
         self.container = ft.Container(content=self.colon, alignment=ft.alignment.center, bgcolor=ft.Colors.SURFACE_CONTAINER_HIGHEST, padding=30, )
 
-        self.v_row = ft.ResponsiveRow([ ft.Column([ self.container ],col={"xs":12, "md":6}) ], alignment=CNTR)
-        super().__init__(controls=[self.v_row], horizontal_alignment=ft.CrossAxisAlignment.CENTER, vertical_alignment=ft.MainAxisAlignment.CENTER, appbar=self.appbar)
+        self.v_row = ft.ResponsiveRow([ft.Column([ft.Container(content=self.colon, bgcolor=ft.Colors.with_opacity(0.1, ft.Colors.WHITE), blur=4, padding=ft.padding.symmetric(50, 120))], col={"xs":12, "md":6}, alignment=CNTR, horizontal_alignment=CX_CENTER)], alignment=ft.MainAxisAlignment.CENTER, expand=True)
+
+        super().__init__(padding=0,controls=[ft.Container(content=self.v_row, expand=True, image=ft.DecorationImage(src="./carbon.jpg", opacity=0.5, fit=ft.ImageFit.COVER),alignment=ft.Alignment(x=0.5, y=0.5))], horizontal_alignment=ft.CrossAxisAlignment.CENTER, vertical_alignment=ft.MainAxisAlignment.CENTER, appbar=self.appbar)
 
     # mainly used in Appbar
     def __str__(self):
